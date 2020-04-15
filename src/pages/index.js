b7167cebe189fe40e6d3bfb71f36674b7b51b948
5aa <yourwishismysuggestion@gmail.com>
2020-04-14 21:34:46 -0400

    clean up imports

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,9 +11,11 @@ const IndexPage = () => (
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image child='placeholderImage' />
-    </div>
+    {
+    //<div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+    //  <Image child='placeholderImage' />
+    //</div>
+    }
     <Link to='/'>Go Home</Link>
   </Layout>
 )
