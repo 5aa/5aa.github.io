db36009876c8a1bbb5544c3e5067df577cfaf454
5aa <yourwishismysuggestion@gmail.com>
2020-04-17 22:45:46 -0400

    style blog posts

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -2,17 +2,31 @@ import React from 'react'
 import { graphql } from 'gatsby'
 import Layout from '../components/Layout'
 import SEO from '../components/seo'
+import './blog-post.scss'
 
 export default ({ data }) => {
   const post = data.markdownRemark
   return (
     <Layout>
       <SEO title={post.frontmatter.title} />
-      <div>
-        <h1>{post.frontmatter.title}</h1>
-        <div>{post.frontmatter.date}</div>
-        <div dangerouslySetInnerHTML={{ __html: post.html }} />
-      </div>
+      <h1 
+        class='post-title'
+				data-aos='fade-up'
+        data-aos-easing='ease-out-quad'
+        data-aos-duration='800'>{post.frontmatter.title}</h1>
+      <div 
+				class='post-date'
+        data-aos='fade-up'
+        data-aos-easing='ease-out-quad'
+        data-aos-duration='800'>Published {post.frontmatter.date}</div>
+      <div
+        class='post-contents' 
+        data-aos='fade-up'
+        data-aos-easing='ease-out-quad'
+        data-aos-duration='500'
+        data-aos-delay='300'
+        dangerouslySetInnerHTML={{ __html: post.html }} 
+      />
     </Layout>
   )
 }
